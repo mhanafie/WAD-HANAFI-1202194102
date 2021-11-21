33bd9cf86131daea4f1a40a7f8455787feb9f287
mhanafie <87606173+mhanafie@users.noreply.github.com>
2021-11-21 23:31:47 +0700

    Initial Commit

---
+++ MODUL3HANAFI/modul3.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Nov 2021 pada 10.58
+-- Versi server: 10.4.8-MariaDB
+-- Versi PHP: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `modul3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku_table`
+--
+
+CREATE TABLE `buku_table` (
+  `id_buku` int(100) NOT NULL,
+  `judul_buku` varchar(255) NOT NULL,
+  `penulis_buku` varchar(255) NOT NULL,
+  `tahun_terbit` int(100) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `tag` varchar(255) NOT NULL,
+  `bahasa` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
